73c79e702ad2e8e3530c5c86dd154ca4f09ce090
sistemasintegradosdegestionsig-hue <sistemasintegradosdegestionsig@gmail.com>
2026-08-04 17:55:55 -0500

    Update pqrs.js

--- js/pqrs.js
+++ js/pqrs.js
@@ -1,49 +1,18 @@
-alert("pqrs.js cargó correctamente");
-// ============================================
-// REGISTRO DE PQRS
-// ============================================
+alert("pqrs.js cargó");
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
+
+    alert("DOM cargado");
 
     const formulario = document.getElementById("pqrsForm");
 
-    if (!formulario) return;
+    alert(formulario);
 
-    formulario.addEventListener("submit", async function (e) {
-        console.log("Entró al submit");
+    formulario.addEventListener("submit", function (e) {
 
         e.preventDefault();
 
-        const datos = {
-
-            nombre: document.getElementById("nombre").value,
-            empresa: document.getElementById("empresa").value,
-            correo: document.getElementById("correo").value,
-            telefono: document.getElementById("telefono").value,
-            ciudad: document.getElementById("ciudad").value,
-            factura: document.getElementById("factura").value,
-            producto: document.getElementById("producto").value,
-            tipo: document.getElementById("tipo").value,
-            descripcion: document.getElementById("descripcion").value
-
-        };
-
-        const respuesta = await guardarPQRS(datos);
-
-        if (respuesta.ok) {
-
-            alert(
-                "PQRS registrada correctamente.\n\nRadicado: " +
-                respuesta.radicado
-            );
-
-            formulario.reset();
-
-        } else {
-
-            alert("Error: " + respuesta.mensaje);
-
-        }
+        alert("El botón funciona");
 
     });
 
