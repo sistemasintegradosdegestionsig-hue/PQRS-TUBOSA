01c3a6db0cbcd8b50f7f9b217d6538d681a25608
sistemasintegradosdegestionsig-hue <sistemasintegradosdegestionsig@gmail.com>
2026-08-04 17:40:21 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,38 @@
+// ============================================
+// API - SISTEMA PQRS TUBOSA
+// ============================================
 
+const API_URL = "https://script.google.com/macros/s/AKfycbycyf0jD5sBDVASB2o5U2SmGOJyAGLaaY5gW5MTCTHqijRtVb3H45ggL-d2uKxgY-AcLg/exec";
+
+async function guardarPQRS(datos) {
+
+    try {
+
+        const respuesta = await fetch(API_URL + "?action=guardarPQRS", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify(datos)
+
+        });
+
+        const resultado = await respuesta.json();
+
+        return resultado;
+
+    } catch (error) {
+
+        console.error(error);
+
+        return {
+            ok: false,
+            mensaje: error.message
+        };
+
+    }
+
+}
